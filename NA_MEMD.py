a245b8130923141ec3ea912424a312056f33f3b8
ethan <ethan.lee.zhu@gmail.com>
2024-07-08 17:52:47 -0400

    Finish Prototype noise.py

--- NA_MEMD.py
+++ NA_MEMD.py
@@ -5,9 +5,16 @@ def na_memd(signals):
 
     channel_count = signals.shape[0]
     sample_count = signals.shape[1]
-    for i in range(channel_count):
 
-        signals[i] = add_noise(signals, method='na_var')
+    noisy_signal1 = add_noise(signals, method='na_fix')
+    print("success", noisy_signal1.shape)
+    noisy_signal2 = add_noise(signals, method='na_snr')
+    print("success", noisy_signal2.shape)
+    noisy_signal3 = add_noise(signals, method='na_var')
+    print("success", noisy_signal3.shape)
+    noisy_signal4 = add_noise(signals, method='na_ran')
+    print("success", noisy_signal4.shape)
+
 
 
 if __name__ == "__main__":
--- noise.py
+++ noise.py
@@ -1,11 +1,26 @@
 import numpy as np
 
-
+# Available methods : na_fix, na_snr, na_var, na_ran
 def add_noise(signal, method='na_fix', intensity=0.1, add_rchannel=None):
-    # available methods : na_fix, na_snr, na_var, na_ran
+    """
+    Add white Gaussian noise to signals
 
-    channel_count = signal.shape[0]
-    sample_count = signal.shape[1]
+    Parameters :
+    - signal (ndarray): signal input consisting of r rows and c columns where each row represents an input channel
+                        and each column represents sample data.
+    - method (str) : method of noise to apply to signal.
+        na_fix : fixed noise
+        na_snr : signal-to-noise ratio
+        na_var : variance based noise
+        na_ran : random noise
+    - intensity (float) : intensity of noise (if using na_snr then intensity becomes desired SNR ratio)
+    - add_rchannel (int) : number of noise channels to add to signals (default set to number of input channels)
+
+    Output :
+    returns ndarray consisting of c + add_rchannel channels of length r
+    """
+
+    channel_count, sample_count = signal.shape
 
     if add_rchannel is None:
         add_rchannel = channel_count
@@ -18,19 +33,24 @@ def add_noise(signal, method='na_fix', intensity=0.1, add_rchannel=None):
         return output
 
     elif method == 'na_snr':
-        pass
+        sig_power = np.sum(np.abs(signal) ** 2)/sample_count
+        noise_add = noise * np.sqrt(sig_power/(10**(intensity/10)))
+        output = np.vstack((signal, noise_add))
+        return output
 
-    elif method == 'na_var':
-        var_noise = np.var(a=signal, axis=1, keepdims=True) * intensity
-        var_noise = noise * var_noise
+    elif method == 'na_var':    # sqrt or no sqrt???
+        var_noise = np.var(a=signal, axis=1, keepdims=True)
+        var_noise = noise * var_noise * intensity
         output = np.vstack((signal, var_noise))
         return output
 
     elif method == 'na_ran':
-        pass
+        rand_factor = np.random.rand(1, sample_count)
+        ran_noise = rand_factor * intensity * noise
+        output = np.vstack((signal, ran_noise))
+        return output
 
     else:
         raise ValueError('\nInvalid method. \n'
                          'Available methods: na_fix, na_snr, na_var, na_ran')
 
-    raise ValueError('Error, Review Input')
