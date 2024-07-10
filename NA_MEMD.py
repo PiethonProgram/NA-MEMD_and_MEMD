41bc7eb6372b416f0cb2fe238b2c76f68577efc2
ethan <ethan.lee.zhu@gmail.com>
2024-07-10 11:07:40 -0400

    reformat, hamm, peaks

--- NA_MEMD.py
+++ NA_MEMD.py
@@ -1,5 +1,5 @@
 from noise import *
-
+from calculations import *
 
 def na_memd(signal,n_dir=50,stop_crit='stop', stop_vect=(0.075, 0.75, 0.075), n_iter=2, n_imf=None,
             method='memd', intensity=0.1, add_rchannel=None):
@@ -10,15 +10,76 @@ def na_memd(signal,n_dir=50,stop_crit='stop', stop_vect=(0.075, 0.75, 0.075), n_
     return (imfs)
 
 
-def memd(*args):
-    x, n_dir, stop_crit, stop_vec, n_iter, = input_check(args)
-    seq, t,
-    signal = x
-    return 1
+def memd(signal, n_dir=50, stop_crit='stop', stop_vec=(0.075, 0.75, 0.075), n_iter=2, n_imf=100):
+    seq, t, nbit, MAXITERATIONS, N_dim, N = initialize_parameters(signal, n_dir)
+    sd, sd2, tol = stop_vec
+
+    n_imf = 1
+    q = []
+
+    while not stop_emd(signal, seq, n_dir, N_dim):
+        # Current mode
+        m = signal
+
+        # Compute mean and stopping criterion
+        if stop_crit == 'stop':
+            stop_sift, env_mean = stop(m, t, sd, sd2, tol, seq, n_dir, N, N_dim)
+        else:
+            counter = 0
+            stop_sift, env_mean, counter = fix(m, t, seq, n_dir, n_iter, counter, N, N_dim)
+
+        # Check for too small amplitude
+        if np.max(np.abs(m)) < (1e-10) * np.max(np.abs(signal)):
+            print('Forced stop of EMD: too small amplitude')
+            break
+
+        # Sifting loop
+        while not stop_sift and nbit < MAXITERATIONS:
+            # Sifting
+            m -= env_mean
+
+            # Compute mean and stopping criterion
+            if stop_crit == 'stop':
+                stop_sift, env_mean = stop(m, t, sd, sd2, tol, seq, n_dir, N, N_dim)
+            else:
+                stop_sift, env_mean, counter = fix(m, t, seq, n_dir, n_iter, counter, N, N_dim)
+
+            nbit += 1
+
+            if nbit == (MAXITERATIONS - 1) and nbit > 100:
+                print('Forced stop of sifting: too many iterations')
+
+        q.append(m.T)
+        signal -= m
+        nbit = 0
+
+    # Store the residue
+    q.append(signal.T)
+    q = np.asarray(q)
+    return q
 
 
-def input_check(*args):
-    pass
+def initialize_parameters(signal, n_dir):
+    N_dim = signal.shape[0]  # Number of channels
+    N = signal.shape[1]  # Number of data points
+
+    base = [-n_dir]
+    if N_dim == 3:
+        base.append(2)
+    else:
+        prm = nth_prime(N_dim - 1)
+        for i in range(1, N_dim):
+            base.append(prm[i - 1])
+
+    seq = np.zeros((n_dir, N_dim - 1 if N_dim == 3 else N_dim))
+    for i in range(len(base)):
+        seq[:, i] = hamm(n_dir, base[i])
+
+    t = np.arange(1, N + 1)
+    nbit = 0
+    MAXITERATIONS = 1000
+
+    return seq, t, nbit, MAXITERATIONS, N_dim, N
 
 
 if __name__ == "__main__":
--- calculations.py
+++ calculations.py
@@ -1,42 +1,89 @@
-# Hammersley an prime calculations
+# Hammersley a prime calculations
 import numpy as np
 
-def hammersley():
-    pass
+
+def hamm(n_dir, base):
+    seq = np.zeros(n_dir)
+    if base > 1:
+        seed = np.arange(1, n_dir + 1)
+        base_inv = 1 / base
+        while np.any(seed != 0):
+            digit = np.remainder(seed, base)
+            seq += digit * base_inv
+            base_inv /= base
+            seed = np.floor(seed / base)
+    else:
+        temp = np.arange(1, n_dir + 1)
+        seq = (np.remainder(temp, -base + 1) + 0.5) / -base
+    return seq
 
 
-def nth_prime(nth):     # 46,000 times faster than other packages
-    if nth >= 0 :
-        if nth <= 5 :
-            return (small_primes(nth))
-        else :
-            return (large_primes(nth))
-    else :
-        raise ValueError("n must be >= 1 for for list of n prime numbers")
+def nth_prime(nth):  # Calculate and return the first nth primes.
+    if nth > 0:
+        if nth <= 5:    # call corner cases
+            return small_primes(nth)
+        else:
+            return large_primes(nth)    # call general cases
+    else:
+        raise ValueError("n must be >= 1 for list of n prime numbers")
 
 
-def small_primes(nth):
+def small_primes(nth):      # corner cases for nth prime estimation using log
     list_prime = np.array([2, 3, 5, 7, 11])
-    return (list_prime[:nth])
+    return list_prime[:nth]
 
 
 def large_primes(nth):
     # https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188
-
     lim = int(nth * (np.log(nth) + np.log(np.log(nth)))) + 1
-
     sieve = np.ones(lim // 3 + (lim % 6 == 2), dtype=bool)
     for i in range(1, int(lim ** 0.5) // 3 + 1):
         if sieve[i]:
             k = 3 * i + 1 | 1
             sieve[k * k // 3::2 * k] = False
             sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = False
-    return (np.r_[2, 3, ((3 * np.nonzero(sieve)[0][1:] + 1) | 1)])
+    return np.r_[2, 3, ((3 * np.nonzero(sieve)[0][1:] + 1) | 1)]
 
 
+def local_peaks(x):
+    if np.all(x < 1e-5):
+        x = np.zeros(len(x))
+
+    m = len(x) - 1
+
+    # Calculates the extrema of the projected signal
+    dy = np.diff(x)
+    a = np.where(dy != 0)[0]
+    lm = np.where(np.diff(a) != 1)[0] + 1
+    d = a[lm] - a[lm - 1]
+    a[lm] = a[lm] - np.floor(d / 2)
+    a = np.append(a, m)
+    ya = x[a]
+
+    if len(ya) > 1:
+        # Calculate differences for peak detection
+        dya = np.diff(ya)
+        sign_change = np.sign(dya)
+
+        # Find local maxima
+        loc_max = np.where((sign_change[:-1] > 0) & (sign_change[1:] < 0))[0] + 1
+        indmax = a[loc_max] if len(loc_max) > 0 else np.array([])
+
+        # Find local minima
+        loc_min = np.where((sign_change[:-1] < 0) & (sign_change[1:] > 0))[0] + 1
+        indmin = a[loc_min] if len(loc_min) > 0 else np.array([])
+    else:
+        indmin = np.array([])
+        indmax = np.array([])
+
+    return indmin, indmax
+
+
+"""
 def is_prime(num):
     i = 2
     while i**2 <= num:
         if num % i == 0: return False
         i += 1
     return True
+"""
