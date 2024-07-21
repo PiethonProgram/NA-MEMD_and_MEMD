54dad12785ba10f9ff1ce57affadd35ab243424f
ethan <ethan.lee.zhu@gmail.com>
2024-07-21 17:39:01 -0400

    update of functions

--- example.py
+++ example.py
@@ -3,10 +3,18 @@ import time
 
 
 np.random.seed(15)
-signal = np.random.randn(3, 1000)
+signal = np.random.rand(5, 1000)
 st = time.time()
-imfs = na_memd(signal, method="na_fix", stop_crit='stop')
+imfs = na_memd(signal, method='na_fix', stop_crit='stop')
 en = time.time()
 net = en-st
 print("time taken : ", net)
 print(imfs.shape)
+
+
+# Future Updates
+#   - Energy Criterion, Residual Criterion
+#   - linear interpolation (interp1d) option
+#   - speed-up using numba
+#   - general speedup of programs
+
